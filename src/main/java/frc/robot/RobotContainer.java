33930ebde7a0bf33b084f1dd54052ce5a7614bd8
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2026-01-22 15:27:54 -0500

    feat: shuffleboard display

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,8 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import edu.wpi.first.wpilibj.DriverStation;
+import static edu.wpi.first.wpilibj.GenericHID.RumbleType.kBothRumble;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -56,13 +58,16 @@ public class RobotContainer {
             )
         );
         joystick.x().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+        // Run SysId routines when holding back/start and X/Y.
+        // Note that each routine should be run exactly once in a single log.
         joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
         joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        joystick.setRumble(kBothRumble,0.5);
         }
         
-
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
         final var idle = new SwerveRequest.Idle();
@@ -75,13 +80,9 @@ public class RobotContainer {
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
         ));
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
         
 
         // reset the field-centric heading on left bumper press
-        
-
         joystick.y().whileTrue(new InstantCommand(() -> {
             System.out.println("pressed x");
             Vision.Cameras.MAIN.updateUnreadResults();
@@ -91,10 +92,11 @@ public class RobotContainer {
             if (result.hasTargets()) {
                 var target = result.getBestTarget();
                 var transform = Vision.getRobotRelativeTransformTo(target);
-                System.out.println("Best target ID: " + target.getFiducialId());
-                System.out.println("Robot-relative transform to target: " + transform);
+                // System.out.println("Best target ID: " + target.getFiducialId());
+                 Shuffleboard.getTab("Vision").addNumber("Best target ID:", () -> target.getFiducialId()); 
+                // System.out.println("Robot-relative transform to target: " + transform);
             } else {
-                System.out.println("No targets seen");
+                // System.out.println("No targets seen");
         }
             // Vision.Cameras.MAIN.getBestResult()
             //   .map(e -> (e.hasTargets() ? e.getBestTarget() : null))

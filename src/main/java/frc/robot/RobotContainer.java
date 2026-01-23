fe7c4eb522701a81c286760013505cc79dffd3e3
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2026-01-23 17:00:23 -0500

    GetVisionData, but not working yet

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,14 +12,13 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import edu.wpi.first.wpilibj.DriverStation;
-import static edu.wpi.first.wpilibj.GenericHID.RumbleType.kBothRumble;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.commands.vision.GetVisionData;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Vision;
@@ -65,7 +64,7 @@ public class RobotContainer {
         joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-        joystick.setRumble(kBothRumble,0.5);
+        // joystick.setRumble(kBothRumble,0.5);
         }
         
         // Idle while the robot is disabled. This ensures the configured
@@ -80,29 +79,52 @@ public class RobotContainer {
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
         ));
 
+        joystick.y().onTrue(new InstantCommand(() -> {        
+            Vision.Cameras.MAIN.updateUnreadResults();
+            var results = Vision.Cameras.MAIN.getCamera().getAllUnreadResults();    
+            if(results.isEmpty()){
+                System.out.println("No targets detected.");
+            }
+            else{
+                var result = results.get(0);
+                // At least one AprilTag was seen by the camera
+                if (result.hasTargets()) {
+                    System.out.println("Targets detected.");
+                    System.out.println(result.getBestTarget().fiducialId);
+                    // System.out.println(result.metadata.captureTimestampMicros);
+                    }
+                else{
+                    System.out.println("What happend?");
+                    // System.out.println(result.metadata.captureTimestampMicros);
+                }
+            }            
+        }));
+
+        joystick.x().onTrue(new GetVisionData());
+
         
 
         // reset the field-centric heading on left bumper press
-        joystick.y().whileTrue(new InstantCommand(() -> {
-            System.out.println("pressed x");
-            Vision.Cameras.MAIN.updateUnreadResults();
-            var results = Vision.Cameras.MAIN.getCamera().getAllUnreadResults();
-            var result = results.get(results.size() - 1);
-                // At least one AprilTag was seen by the camera
-            if (result.hasTargets()) {
-                var target = result.getBestTarget();
-                var transform = Vision.getRobotRelativeTransformTo(target);
-                // System.out.println("Best target ID: " + target.getFiducialId());
-                 Shuffleboard.getTab("Vision").addNumber("Best target ID:", () -> target.getFiducialId()); 
-                // System.out.println("Robot-relative transform to target: " + transform);
-            } else {
-                // System.out.println("No targets seen");
-        }
-            // Vision.Cameras.MAIN.getBestResult()
-            //   .map(e -> (e.hasTargets() ? e.getBestTarget() : null))
-            //   .map(Vision::getRobotRelativeTransformTo)
-            //   .ifPresent(System.out::println);
-        }).repeatedly());
+        // joystick.y().whileTrue(new InstantCommand(() -> {
+        //     System.out.println("pressed x");
+        //     Vision.Cameras.MAIN.updateUnreadResults();
+        //     var results = Vision.Cameras.MAIN.getCamera().getAllUnreadResults();
+        //     var result = results.get(results.size() - 1);
+        //         // At least one AprilTag was seen by the camera
+        //     if (result.hasTargets()) {
+        //         var target = result.getBestTarget();
+        //         var transform = Vision.getRobotRelativeTransformTo(target);
+        //         // System.out.println("Best target ID: " + target.getFiducialId());
+        //          Shuffleboard.getTab("Vision").addNumber("Best target ID:", () -> target.getFiducialId()); 
+        //         // System.out.println("Robot-relative transform to target: " + transform);
+        //     } else {
+        //         // System.out.println("No targets seen");
+        // }
+        //     // Vision.Cameras.MAIN.getBestResult()
+        //     //   .map(e -> (e.hasTargets() ? e.getBestTarget() : null))
+        //     //   .map(Vision::getRobotRelativeTransformTo)
+        //     //   .ifPresent(System.out::println);
+        // }).repeatedly());
         
         
         drivetrain.registerTelemetry(logger::telemeterize);
---
+++ src/main/java/frc/robot/commands/vision/GetVisionData.java
@@ -0,0 +1,46 @@
+package frc.robot.commands.vision;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Vision;
+
+public class GetVisionData extends Command {
+    private boolean targetSet = false;
+    private int tagID = 0;
+
+    public GetVisionData() {
+
+    }
+
+    @Override
+    public void initialize() {
+        targetSet = false;
+        tagID = 0;
+    }
+
+    @Override
+    public void execute() {
+        Shuffleboard.getTab("Vision").addNumber("Tag ID", () -> tagID);
+        Shuffleboard.getTab("Vision").addBoolean("Has Target", () -> targetSet);
+        Vision.Cameras.MAIN.updateUnreadResults();
+        var results = Vision.Cameras.MAIN.getLatestResult();
+        
+        if (!results.isEmpty()) {
+            targetSet = true;
+            tagID = results.get().getBestTarget().getFiducialId();
+        } else {
+            targetSet = false;
+            tagID = -1;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println("Target Found!");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return targetSet;
+    }
+}

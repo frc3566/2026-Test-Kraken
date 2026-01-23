b2287a72f7c983d1ec6714597f547d8878fda5b3
zerrrr0 <152433533+zerrrr0@users.noreply.github.com>
2026-01-23 16:26:52 -0500

    Shooter subsystem
    Add shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+  private TalonFX left= new TalonFX(Constants.LEFT_MOTOR);
+  private TalonFX right= new TalonFX(Constants.RIGHT_MOTOR);
+  public void setPower(double speed){
+    left.set(speed);
+    right.set(speed);
+  }
+  public void stop(){
+    left.stopMotor();
+    right.stopMotor();
+  }
+//   public void setVoltage(double voltage){
+//     left.setVoltage(voltage);
+//     right.setVoltage(voltage);
+//   }
+}

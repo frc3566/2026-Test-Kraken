ffdcd3e2af225e03c8ee951fc785c0af5d07394d
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2026-02-20 22:27:58 -0500

    fix?: robot moving in sim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,12 +8,14 @@ import com.ctre.phoenix6.HootAutoReplay;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
+    private final Field2d field = new Field2d();
 
     private final RobotContainer m_robotContainer;
 
@@ -94,6 +96,8 @@ public class Robot extends TimedRobot {
 
     @Override
     public void simulationPeriodic() {
+        field.setRobotPose(m_robotContainer.drivetrain.getState().Pose);
+        SmartDashboard.putData("Field", field);
     }
 
     public void setTeleopShifts(){

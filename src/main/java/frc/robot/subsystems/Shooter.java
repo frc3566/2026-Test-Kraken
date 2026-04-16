f5004b3b3bf86f8fcb37c9f9a5bb925e63a57b06
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2026-04-16 10:13:53 -0400

    fix: autopower equation

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -116,7 +116,8 @@ public class Shooter extends SubsystemBase {
         // double distFeet = Units.metersToFeet(distance);
         // double rps = 39.7+ 0.0417 * distFeet + 0.137 * Math.pow(distFeet,2)-4;
         // TODO: Redo the equation
-        double rps = -1.3664 * Math.pow(distance,2) + 10.591*distance + 28.047;
+        // double rps = -1.3664 * Math.pow(distance,2) + 10.591*distance + 28.047;
+        double rps = 9.0279*Math.log(distance) + 37.518;
         return Math.min(100,rps); 
     }
 
